970b17b3db9089e8662db70110b62c2d95772526
ady243 <masiviady7@gmail.com>
2024-09-20 21:00:50 +0200

    storage.go

--- storage/storage.go
+++ storage/storage.go
@@ -22,13 +22,12 @@ func NewConnection() (*gorm.DB, error) {
 
 	// Postgres connection string
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Paris",
-    os.Getenv("POSTGRES_HOST"),      
-    os.Getenv("POSTGRES_USER"),
-    os.Getenv("POSTGRES_PASSWORD"),
-    os.Getenv("POSTGRES_DB"),
-    os.Getenv("POSTGRES_PORT"),
-)
-
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+		os.Getenv("POSTGRES_PORT"),
+	)
 
 	// Connect to Postgres using GORM
 	GDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
