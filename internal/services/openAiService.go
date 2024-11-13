48230610a9472b34f5605774c5eefe867bfe0c57
Ady <masiviady7@gmail.com>
2024-11-13 16:13:00 +0100

    update backend

--- internal/services/openAiService.go
+++ internal/services/openAiService.go
@@ -1,66 +1,66 @@
 package services
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "os"
+	"context"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/ady243/teamup/internal/models"
-    openai "github.com/sashabaranov/go-openai"
+	"github.com/ady243/teamup/internal/models"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 type OpenAIService struct {
-    client *openai.Client
+	client *openai.Client
 }
 
 func NewOpenAIService() *OpenAIService {
-    apiKey := os.Getenv("OPENAI_API_KEY")
-    if apiKey == "" {
-        log.Fatal("OPENAI_API_KEY environment variable is not set")
-    }
-    client := openai.NewClient(apiKey)
-    return &OpenAIService{
-        client: client,
-    }
+	apiKey := os.Getenv("OPENAI_API_KEY")
+	if apiKey == "" {
+		log.Fatal("OPENAI_API_KEY environment variable is not set")
+	}
+	client := openai.NewClient(apiKey)
+	return &OpenAIService{
+		client: client,
+	}
 }
 
 func (s *OpenAIService) SuggestFormations(players []models.Users) ([]string, error) {
-    var formations []string
+	var formations []string
 
-    //Ici on fait appel à l'API OpenAI pour suggérer une formation
-    prompt := "Voici les statistiques des joueurs :\n"
-    for _, player := range players {
-        prompt += fmt.Sprintf("Joueur: %s, Pac: %d, Sho: %d, Pas: %d, Dri: %d, Def: %d, Phy: %d\n",
-            player.Username, player.Pac, player.Sho, player.Pas, player.Dri, player.Def, player.Phy)
-    }
-    prompt += "Suggérez une formation pour ces joueurs."
-    // Ici on fait la gestion des erreurs et on initialise la version du modèle à utiliser
-    resp, err := s.client.CreateChatCompletion(context.Background(), openai.ChatCompletionRequest{
-        Model: "gpt-3.5-turbo",
-        Messages: []openai.ChatCompletionMessage{
-            {
-                Role:    "system",
-                Content: "You are a football coach assistant.",
-            },
-            {
-                Role:    "user",
-                Content: prompt,
-            },
-        },
-        MaxTokens: 100,
-    })
-    if err != nil {
-        log.Printf("Erreur lors de l'appel à l'API OpenAI: %v", err)
-        return nil, err
-    }
+	//Ici on fait appel à l'API OpenAI pour suggérer une formation
+	prompt := "Voici les statistiques des joueurs de football :\n"
+	for _, player := range players {
+		prompt += fmt.Sprintf("Joueur: %s, Pac: %d, Sho: %d, Pas: %d, Dri: %d, Def: %d, Phy: %d\n",
+			player.Username, player.Pac, player.Sho, player.Pas, player.Dri, player.Def, player.Phy)
+	}
+	prompt += "Donne moi la formation la plus adaptée suivant leurs profil par exemple Formation : 4-4-2. Pour chacun des joueurs de la liste, donne moi sa positions idéale sur le terrain, par exemple : Joueur : Username, Position : Defenseur gauche."
+	// Ici on fait la gestion des erreurs et on initialise la version du modèle à utiliser
+	resp, err := s.client.CreateChatCompletion(context.Background(), openai.ChatCompletionRequest{
+		Model: "gpt-4o-mini",
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    "system",
+				Content: "You are a football coach assistant.",
+			},
+			{
+				Role:    "user",
+				Content: prompt,
+			},
+		},
+		MaxTokens: 100,
+	})
+	if err != nil {
+		log.Printf("Erreur lors de l'appel à l'API OpenAI: %v", err)
+		return nil, err
+	}
 
-    // Ici on extrait la formation suggérée par OpenAI
-    if len(resp.Choices) > 0 {
-        formations = append(formations, resp.Choices[0].Message.Content)
-    } else {
-        return nil, fmt.Errorf("aucune formation suggérée par OpenAI")
-    }
+	// Ici on extrait la formation suggérée par OpenAI
+	if len(resp.Choices) > 0 {
+		formations = append(formations, resp.Choices[0].Message.Content)
+	} else {
+		return nil, fmt.Errorf("aucune formation suggérée par OpenAI")
+	}
 
-    return formations, nil
+	return formations, nil
 }
