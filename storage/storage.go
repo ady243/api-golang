b6f1b748e7b43c0bbe150ace2add0b6370412430
ady243 <masiviady7@gmail.com>
2024-11-22 12:21:14 +0100

    fix that

--- storage/storage.go
+++ storage/storage.go
@@ -21,7 +21,8 @@ func NewConnection() (*gorm.DB, error) {
 	}
 
 	// Postgres connection string
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Paris",
+	// Postgres connection string
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Europe/Paris",
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PASSWORD"),
