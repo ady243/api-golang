d38f72963c4e4a70214bda1415153a9074e03e32
ady243 <masiviady7@gmail.com>
2024-11-22 19:55:40 +0100

    refix swagger

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,7 +4,7 @@ import (
     "log"
     "os"
 
-    _ "github.com/ady243/teamup/docs" 
+    _ "github.com/ady243/teamup/docs"
     "github.com/ady243/teamup/internal/controllers"
     "github.com/ady243/teamup/internal/models"
     "github.com/ady243/teamup/internal/routes"
@@ -20,76 +20,76 @@ import (
 )
 
 func Run() {
-	// Load environment variables
-	if err := godotenv.Load(".env"); err != nil {
-		log.Println("No .env file found", err)
-	}
+    // Load environment variables
+    if err := godotenv.Load(".env"); err != nil {
+        log.Println("No .env file found", err)
+    }
 
-	// Database connection
-	db, err := storage.NewConnection()
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+    // Database connection
+    db, err := storage.NewConnection()
+    if err != nil {
+        log.Fatalf("Failed to connect to database: %v", err)
+    }
 
-	// Table migration
-	if err := db.AutoMigrate(&models.Users{}, &models.Matches{}, &models.MatchPlayers{}); err != nil {
-		log.Printf("Error migrating database: %v", err)
-	}
+    // Table migration
+    if err := db.AutoMigrate(&models.Users{}, &models.Matches{}, &models.MatchPlayers{}); err != nil {
+        log.Printf("Error migrating database: %v", err)
+    }
 
-	// Optional: Create fake users for testing
-	// users := models.GenerateFakeUsers(10)
-	// for _, user := range users {
-	//     db.Create(&user)
-	// }
+    // Optional: Create fake users for testing
+    // users := models.GenerateFakeUsers(10)
+    // for _, user := range users {
+    //     db.Create(&user)
+    // }
 
-	// Connect to Redis
-	redisClient := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("REDIS_ADDR"),
-	})
+    // Connect to Redis
+    redisClient := redis.NewClient(&redis.Options{
+        Addr: os.Getenv("REDIS_ADDR"),
+    })
 
-	// Initialize services and controllers
-	imageService := services.NewImageService("./uploads")
-	authService := services.NewAuthService(db, imageService, services.NewEmailService())
-	authController := controllers.NewAuthController(authService, imageService)
-	matchService := services.NewMatchService(db)
-	openAIService := services.NewOpenAIService()
-	chatService := services.NewChatService(db, redisClient)
-	matchController := controllers.NewMatchController(matchService, authService, db, chatService)
-	matchPlayersService := services.NewMatchPlayersService(db)
-	matchPlayersController := controllers.NewMatchPlayersController(matchPlayersService, authService, db)
-	chatController := controllers.NewChatController(chatService)
-	openAiController := controllers.NewOpenAiController(openAIService, matchPlayersService)
+    // Initialize services and controllers
+    imageService := services.NewImageService("./uploads")
+    authService := services.NewAuthService(db, imageService, services.NewEmailService())
+    authController := controllers.NewAuthController(authService, imageService)
+    matchService := services.NewMatchService(db)
+    openAIService := services.NewOpenAIService()
+    chatService := services.NewChatService(db, redisClient)
+    matchController := controllers.NewMatchController(matchService, authService, db, chatService)
+    matchPlayersService := services.NewMatchPlayersService(db)
+    matchPlayersController := controllers.NewMatchPlayersController(matchPlayersService, authService, db)
+    chatController := controllers.NewChatController(chatService)
+    openAiController := controllers.NewOpenAiController(openAIService, matchPlayersService)
 
-	// Configure Fiber app
-	app := fiber.New()
-	app.Use(helmet.New())
-	app.Use(cors.New(cors.Config{
-		AllowOrigins: "*",
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-	}))
-	app.Use(limiter.New(limiter.Config{
-		Max:        10,
-		Expiration: 30 * 1000,
-	}))
+    // Configure Fiber app
+    app := fiber.New()
+    app.Use(helmet.New())
+    app.Use(cors.New(cors.Config{
+        AllowOrigins: "*",
+        AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+    }))
+    app.Use(limiter.New(limiter.Config{
+        Max:        10,
+        Expiration: 30 * 1000,
+    }))
 
-	// Define routes
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Welcome to TeamUp API!")
-	})
-	routes.SetupRoutesAuth(app, authController)
-	routes.SetupRoutesMatches(app, matchController)
-	routes.SetupRoutesMatchePlayers(app, matchPlayersController)
-	routes.SetupChatRoutes(app, chatController)
-	routes.SetupOpenAiRoutes(app, openAiController)
+    // Define routes
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Welcome to TeamUp API!")
+    })
+    routes.SetupRoutesAuth(app, authController)
+    routes.SetupRoutesMatches(app, matchController)
+    routes.SetupRoutesMatchePlayers(app, matchPlayersController)
+    routes.SetupChatRoutes(app, chatController)
+    routes.SetupOpenAiRoutes(app, openAiController)
 
-	// Swagger route
-	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+    // Swagger route
+    app.Get("/swagger/*", fiberSwagger.WrapHandler)
 
-	// Start server
-	port := os.Getenv("API_PORT")
-	if port == "" {
-		port = "3003"
-	}
-	log.Printf("Server started on port %s", port)
-	log.Fatal(app.Listen(":" + port))
+    // Start server
+    port := os.Getenv("API_PORT")
+    if port == "" {
+        port = "3003"
+    }
+    log.Printf("Server started on port %s", port)
+    log.Fatal(app.Listen(":" + port))
 }
