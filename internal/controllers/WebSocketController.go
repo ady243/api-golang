8a66dfaa8d5ff2847fbe848881b3efacf274e7be
ady243 <masiviady7@gmail.com>
2024-12-25 12:30:51 +0100

    add socket for status

---
+++ internal/controllers/WebSocketController.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+    "github.com/ady243/teamup/internal/services"
+    "github.com/gofiber/websocket/v2"
+)
+
+type WebSocketController struct {
+    service *services.WebSocketService
+}
+
+func NewWebSocketController(service *services.WebSocketService) *WebSocketController {
+    return &WebSocketController{service: service}
+}
+
+func (ctrl *WebSocketController) WebSocketHandler(c *websocket.Conn) {
+    ctrl.service.HandleWebSocket(c)
+}
---
+++ internal/services/WebSocketService.go
@@ -0,0 +1,61 @@
+package services
+
+import (
+	"log"
+	"sync"
+
+	"github.com/gofiber/websocket/v2"
+)
+
+type WebSocketService struct {
+	connections map[*websocket.Conn]bool
+	broadcast   chan []byte
+	mutex       sync.Mutex
+}
+
+func NewWebSocketService() *WebSocketService {
+	return &WebSocketService{
+		connections: make(map[*websocket.Conn]bool),
+		broadcast:   make(chan []byte),
+	}
+}
+
+func (s *WebSocketService) HandleWebSocket(c *websocket.Conn) {
+	s.mutex.Lock()
+	s.connections[c] = true
+	s.mutex.Unlock()
+
+	defer func() {
+		s.mutex.Lock()
+		delete(s.connections, c)
+		s.mutex.Unlock()
+		if err := c.Close(); err != nil {
+			log.Println("Error closing WebSocket connection:", err)
+		}
+	}()
+
+	for {
+		_, msg, err := c.ReadMessage()
+		if err != nil {
+			log.Println("Error reading WebSocket message:", err)
+			break
+		}
+		log.Printf("Received message: %s", msg)
+		s.broadcast <- msg
+	}
+}
+
+func (s *WebSocketService) StartBroadcast() {
+	for {
+		msg := <-s.broadcast
+		s.mutex.Lock()
+		for conn := range s.connections {
+			if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+				log.Println("Error writing WebSocket message:", err)
+				conn.Close()
+				delete(s.connections, conn)
+			}
+		}
+		s.mutex.Unlock()
+	}
+}
