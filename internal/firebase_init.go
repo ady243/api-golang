b8486ae7d32de33464031825f98e8517471bf3b7
ady243 <masiviady7@gmail.com>
2025-01-05 00:39:36 +0100

    agin :/

--- internal/firebase_init.go
+++ internal/firebase_init.go
@@ -13,7 +13,9 @@ func InitializeFirebase() (*firebase.App, error) {
 
 	opt := option.WithCredentialsFile("./fireJson/notification-push-40d24-firebase-adminsdk-yla8e-95ee6971eb.json")
 
-	app, err := firebase.NewApp(ctx, nil, opt)
+	app, err := firebase.NewApp(ctx, &firebase.Config{
+		ProjectID: "notification-push-40d24",
+	}, opt)
 	if err != nil {
 		log.Fatalf("Erreur lors de l'initialisation de Firebase: %v", err)
 		return nil, err
