ca36bcb5e05f4a8c811e995655d4a004ecc80be1
ady243 <masiviady7@gmail.com>
2025-01-05 18:30:51 +0100

    update

--- internal/firebase_init.go
+++ internal/firebase_init.go
@@ -3,7 +3,9 @@ package internal
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
@@ -14,12 +16,17 @@ var app *firebase.App
 func InitializeFirebase() (*firebase.App, error) {
 	credentialsFilePath := "./notification-push-40d24-firebase-adminsdk-yla8e-95ee6971eb.json"
 
-	if _, err := os.Stat(credentialsFilePath); os.IsNotExist(err) {
+	absPath, err := filepath.Abs(credentialsFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("erreur lors de la récupération du chemin absolu: %v", err)
+	}
+	log.Printf("Chemin absolu du fichier de clés de service Firebase: %s", absPath)
+
+	if _, err := os.Stat(absPath); os.IsNotExist(err) {
 		return nil, fmt.Errorf("le fichier de clés de service Firebase n'existe pas: %v", err)
 	}
 
-	opt := option.WithCredentialsFile(credentialsFilePath)
-	var err error
+	opt := option.WithCredentialsFile(absPath)
 	app, err = firebase.NewApp(context.Background(), &firebase.Config{
 		ProjectID: "notification-push-40d24",
 	}, opt)
