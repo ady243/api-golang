71189347dbfb14f3fcd24647323b5749dc8799e9
ady243 <masiviady7@gmail.com>
2025-01-05 17:48:41 +0100

    update id project

--- internal/firebase_init.go
+++ internal/firebase_init.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"context"
-
 	"log"
 
 	firebase "firebase.google.com/go"
@@ -14,7 +13,9 @@ var app *firebase.App
 func init() {
 	opt := option.WithCredentialsFile("./fireJson/notification-push-40d24-firebase-adminsdk-yla8e-95ee6971eb.json")
 	var err error
-	app, err = firebase.NewApp(context.Background(), nil, opt)
+	app, err = firebase.NewApp(context.Background(), &firebase.Config{
+		ProjectID: "notification-push-40d24",
+	}, opt)
 	if err != nil {
 		log.Fatalf("erreur lors de l'initialisation de l'application: %v", err)
 	}
